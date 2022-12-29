68139bfde185a2a7eaeff58b43168fb7480cc690
harsh17112000 <77334392+harsh17112000@users.noreply.github.com>
2022-12-29 23:00:49 +0530

    changes

--- src/components/Headers/Headers.js
+++ src/components/Headers/Headers.js
@@ -2,15 +2,16 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from "react-router-dom"
 
 const Headers = () => {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="dark" variant="dark" style={{ height: "50px" }}>
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <NavLink to="/" className="text-decoration-none text-light mx-2">Navbar</NavLink>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <NavLink to="/" className="text-decoration-none text-light ">Home</NavLink>
           </Nav>
         </Container>
       </Navbar>
